084cf348b33e678e5399430c8be57927d7ef112d
司灵 <jinlincao.cjl@alibaba-inc.com>
2022-01-04 17:19:51 +0800

    leetcode

--- src/main/java/com/leetcode/demo/leetcode/tree/Solution.java
+++ src/main/java/com/leetcode/demo/leetcode/tree/Solution.java
@@ -1,7 +1,9 @@
 package com.leetcode.demo.leetcode.tree;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -189,6 +191,7 @@ public class Solution {
 
     /**
      * 二叉树的堂兄弟节点
+     *
      * @param root
      * @param x
      * @param y
@@ -228,5 +231,33 @@ public class Solution {
             }
         }
         return stack.isEmpty();
+    }
+
+    /**
+     * 后序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {return res;}
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
     }
 }
